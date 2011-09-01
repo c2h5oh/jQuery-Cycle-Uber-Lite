16e70704f91f6d23ec9858894315062c86b3af57
Maciej Lisiewski <m.lisiewski@zerg.pl>
2011-09-02 01:25:42 +0200

    pretty much complete plugin, minus the transition animation

--- jqlite-cycle-uber-lite.js
+++ jqlite-cycle-uber-lite.js
@@ -11,49 +11,113 @@
 (function($) {
 	var opts = new Array;	
 	var img = new Array;
+	var imgInc = new Array;
 	var links = new Array;
 	var titles = new Array;		
 	
 	$.fn.jQLiteCycle = $.fn.jqlitecycle = function(options){
 	
+		//initialize variables
 		init = function(el){
 			opts[el.id] = $.extend({}, $.fn.jQLiteCycle.defaults, options);
 			img[el.id] = new Array(); // array for images
 			links[el.id] = new Array(); // array for image links
-			titles[el.id] = new Array(); // array for link/image titles		
+			titles[el.id] = new Array(); // array for link/image titles			
 			imgInc[el.id] = 0;
-			inc[el.id] = 0;
 		}
 		
+		//fetch images, links, titles, set animation interval
 		fetchImages = function(el){
 			$.each($('#'+el.id+' img'), function(i,item){
 				img[el.id][i] = $(item).attr('src');
 				links[el.id][i] = $(item).next().attr('href');
 				titles[el.id][i] = $(item).attr('alt') ? $(item).attr('alt') : '';
+				
 				$(item).hide();
 			
-				$('.ft-'+el.id).mouseover(function(){
+				$('.jql-'+el.id).mouseover(function(){
 					opts[el.id].pause = true;
 				});
 		
-				$('.ft-'+el.id).mouseout(function(){
+				$('.jql-'+el.id).mouseout(function(){
 					opts[el.id].pause = false;
 				});	
 			
-				$('#ft-title-'+el.id).mouseover(function(){
+				$('#jql-title-'+el.id).mouseover(function(){
 					opts[el.id].pause = true;
 				});
 		
-				$('#ft-title-'+el.id).mouseout(function(){
+				$('#jql-title-'+el.id).mouseout(function(){
 					opts[el.id].pause = false;
 				});
+				
 				clearInterval(imgInt[el.id]);	
 				imgInt[el.id] = setInterval(function() { $.transition(el)  }, params.delay);				
 			});
 		}
+		
 		this.each (
 			function(){ init(this); }
 		);	
+		
+		// navigation
+		$.navigation = function(el){
+			// create prev and next 
+			$('#'+el.id).append("<a href='#' id='jql-prev-"+el.id+"' class='jql-prev'>"+opts.next+"</a>");
+			$('#'+el.id).append("<a href='#' id='jql-next-"+el.id+"' class='jql-next'>"+opts.prev+"</a>");
+		
+			// bind prev and next actions
+			$('#jql-prev-'+el.id).css({
+				'position' 	: 'absolute',
+				'top'		: params.height/2 - 15,
+				'left'		: 0,
+				'z-index' 	: 1001,
+				'line-height': '30px',
+				'opacity'	: 0.7
+			}).click( function(e){
+				e.preventDefault();
+				$.transition(el,'prev');
+				clearInterval(imgInt[el.id]);
+				imgInt[el.id] = setInterval(function() { $.transition(el)  }, opts.delay);		
+			});
+
+			$('#jql-next-'+el.id).css({
+				'position' 	: 'absolute',
+				'top'		: params.height/2 - 15,
+				'right'		: 0,
+				'z-index' 	: 1001,
+				'line-height': '30px',
+				'opacity'	: 0.7
+			}).click( function(e){
+				e.preventDefault();
+				$.transition(el,'next');
+				clearInterval(imgInt[el.id]);
+				imgInt[el.id] = setInterval(function() { $.transition(el)  }, opts.delay);
+			});
+		}
+		
+		$.transition = function(el,direction){
+
+			if(opts[el.id].pause == true) return;			
+		
+			$('#'+el.id).css({ 'background-image': 'url('+img[el.id][imgInc[el.id]]+')' });
+		
+			if(direction == 'next')
+				imgInc[el.id]++;
+			else			
+				imgInc[el.id]--;			
+
+			if  (imgInc[el.id] == img[el.id].length) {
+				imgInc[el.id] = 0;
+			}
+					
+			if (imgInc[el.id] == -1){
+				imgInc[el.id] = img[el.id].length-1;
+			}			
+		
+			inc[el.id] = 0;		
+		};
+
 	}
 	
 	// default values
@@ -61,6 +125,8 @@
 		delay: 5000, // delay between images in ms
 		transitionSpeed: 300, // image transition speed in ms		
 		navigation: false, // prev next and buttons
+		prev: 'prev', // previous button text
+		next: 'next', // next button text
 		links : false, // show images as links 		
 		lazyLoad: false // html to be appended inside container to provide lazy loading of images beyons 1st one 
 	};
